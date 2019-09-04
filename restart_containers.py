773b882ca02e3c07bc16f3d991205262ed624785
Alexander Marquardt <alexander.marquardt@elastic.co>
2019-09-04 15:54:57 +0200

    Renamed

--- restart_containers.py
+++ restart_containers.py
@@ -1,6 +1,10 @@
 # script to be used for restarting ECE components
 import subprocess
 import re
+import time
+import sys
+
+TEST_MODE=True
 
 
 CONTAINER_NAMES = ["frc-allocators-allocator",
@@ -10,20 +14,26 @@ CONTAINER_NAMES = ["frc-allocators-allocator",
                    "frc-zookeeper-servers-zookeeper",
                    "frc-constructors-constructor"]
 
-CONTAINER_NAMES = ["mysql/mysql-server"]
-
+SIMULATE_DOCKER_PS_OUTPUT_FILE = "docker_ps.txt"
 
 def get_docker_ps():
-    sub_proc = subprocess.Popen(["docker", "ps"], stdout=subprocess.PIPE)
-    (response, err) = sub_proc.communicate()
-    return response
+    if not TEST_MODE:
+        sub_proc = subprocess.Popen(["docker", "ps"], stdout=subprocess.PIPE)
+        (response, err) = sub_proc.communicate()
+        return response
 
+    else:
+        f=open(SIMULATE_DOCKER_PS_OUTPUT_FILE, "r")
+        response = f.read()
+        f.close()
 
-def get_container_id_and_status(container_name):
+        return response
+
+def get_container_id_and_full_line(container_name):
     response = get_docker_ps()
     for line in response.splitlines():
         list_of_vals = line.split()
-        if re.search(container_name, list_of_vals[1]):
+        if re.search(container_name, line):
             return list_of_vals[0], line
     return None, None
 
@@ -31,27 +41,29 @@ def wait_for_container_to_restart(container_name):
     print "Waiting for %s to start" % container_name
     healthy = False
     while not healthy:
-        container_id, full_line_from_docker_ps = get_container_id_and_status(container_name)
+        container_id, full_line_from_docker_ps = get_container_id_and_full_line(container_name)
 
         if container_id:
             print "\nFound container %s with id %s - waiting for healthy" % (
                 container_name, container_id)
             while not healthy:
-                container_id, full_line_from_docker_ps = get_container_id_and_status(container_name)
+                container_id, full_line_from_docker_ps = get_container_id_and_full_line(container_name)
                 if re.search("\(healthy\)", full_line_from_docker_ps):
                     print "Healthy container %s with id %s" % (
                         container_name, container_id)
                     healthy = True
                 else:
-                    print ".",
+                    print ".",; sys.stdout.flush(); time.sleep(2)
+
+
         else:
-            print ".",
+            print ".", ; sys.stdout.flush(); time.sleep(2)
 
     print "\nContainer %s is now running and healthy with id: %s!" % (container_name, container_id)
 
 
 def remove_container(container_name):
-    container_id, container_status = get_container_id_and_status(container_name)
+    container_id, full_line_from_docker_ps = get_container_id_and_full_line(container_name)
     if container_id:
         print "Removing %s" % container_id
         subprocess.call(["docker", "rm", "-f", container_id])
@@ -61,7 +73,6 @@ def remove_container(container_name):
 def main():
     for container_name in CONTAINER_NAMES:
         remove_container(container_name)
-
 
 
 if __name__ == '__main__':
